5173043ce52029d2723532240183ef1358415580
Sam Smith <samlsmith424@gmail.com>
2022-03-28 16:33:11 -0700

    Test: can get a merchant by its id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,19 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
-  it 'returns a list of all merchants' do
-    create_list(:merchant, 3)
+  describe 'get all merchants endpoint' do
+    it 'returns a list of all merchants' do
+      create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+      get '/api/v1/merchants'
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants.count).to eq(3)
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(merchants.count).to eq(3)
 
-    merchants.each do |merchant|
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(Integer)
+
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a(String)
+      end
+    end
+  end
+
+  describe 'get one merchant endpoint' do
+    it 'can get one merchant by its id' do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to eq(id)
 
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
