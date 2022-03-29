a01638006efb06b8c0cdee517a0f427792453f7b
Sam Smith <samlsmith424@gmail.com>
2022-03-29 12:22:01 -0700

    Test: merchant items api end point

--- spec/requests/api/v1/merchant_items_spec.rb
+++ spec/requests/api/v1/merchant_items_spec.rb
@@ -1,17 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Items API' do
-  xit 'gets all items for a given merchant id' do
+  it 'gets all items for a given merchant id' do
     merchant1 = create(:merchant)
     items = create_list(:item, 3, merchant_id: merchant1.id)
 
-    # get "/merchants/#{merchant1.id}/items"
     get api_v1_merchant_items_path(merchant1.id)
 
     merchant_items = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    # need to test merchant items count to be 3
-    # have item attributes and merchant1 id
+    expect(merchant_items[:data].count).to eq(3)
+
+    merchant_items[:data].each do |merchant_item|
+      expect(merchant_item).to have_key(:id)
+      expect(merchant_item[:id]).to be_a(String)
+
+      expect(merchant_item).to have_key(:attributes)
+      expect(merchant_item[:attributes]).to be_a(Hash)
+
+      expect(merchant_item[:attributes]).to have_key(:name)
+      expect(merchant_item[:attributes][:name]).to be_a(String)
+
+      expect(merchant_item[:attributes]).to have_key(:description)
+      expect(merchant_item[:attributes][:description]).to be_a(String)
+
+      expect(merchant_item[:attributes]).to have_key(:unit_price)
+      expect(merchant_item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(merchant_item[:attributes]).to have_key(:merchant_id)
+      expect(merchant_item[:attributes][:merchant_id]).to be_a(Integer)
+      expect(merchant_item[:attributes][:merchant_id]).to eq(merchant1.id)
+    end
   end
 end
