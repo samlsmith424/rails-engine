29dae7c3973d41a106583ab55e61b20f01607a9f
Sam Smith <samlsmith424@gmail.com>
2022-03-31 08:34:05 -0700

    Test: update item sad path

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -22,7 +22,8 @@ class Api::V1::ItemsController < ApplicationController
     if item.save
       render json: ItemSerializer.new(item)
     else
-      render status: :bad_request
+      render json: { message: 'Unable to update item'}, status: :bad_request
+      # render status: :bad_request
     end
   end
 
