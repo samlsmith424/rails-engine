2ad079bc5d3a56e430ab91ad472b2a2ef8eee3e9
Sam Smith <samlsmith424@gmail.com>
2022-03-31 23:03:51 -0700

    Feat: refactor white space

--- app/controllers/api/v1/item_search_controller.rb
+++ app/controllers/api/v1/item_search_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::ItemSearchController < ApplicationController
   def index
     if params[:name] == ""
-      render json: { data: {message: 'No search matches, please try again'} }, status: 400
+      render json: { data: {message: 'No search matches, please try again'} }, status: :bad_request
     elsif params[:name]
       items = Item.find_all_items(params[:name])
       if items == nil
-        render json: { data: {message: 'No search matches, please try again'} }, status: 400
+        render json: { data: {message: 'No search matches, please try again'} }, status: :bad_request
       else
         render json: ItemSerializer.new(items)
       end
     elsif params[:name] == nil
-      render json: { data: {message: 'No search matches, please try again'} }, status: 400
+      render json: { data: {message: 'No search matches, please try again'} }, status: :bad_request
     else
       render json: ItemSerializer.new(Item.find_all_items(params[:name]))
     end
