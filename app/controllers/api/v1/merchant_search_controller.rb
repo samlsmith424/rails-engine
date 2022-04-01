9a9927d8d66c6a4a818ffbb32e769cf7b08babcd
Sam Smith <samlsmith424@gmail.com>
2022-03-31 20:43:28 -0700

    Feat: refactor whitespace

--- app/controllers/api/v1/merchant_search_controller.rb
+++ app/controllers/api/v1/merchant_search_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::MerchantSearchController < ApplicationController
   def show
     if params[:name] == ""
       render json: { data: {message: 'No search matches, please try again'} }, status: 400
-      
     elsif params[:name]
       merchant = Merchant.search(params[:name])
       if merchant == nil
@@ -10,9 +9,6 @@ class Api::V1::MerchantSearchController < ApplicationController
       else
         render json: MerchantSerializer.new(merchant)
       end
-    # elsif params[:name] == ""
-    #   require "pry"; binding.pry
-    #   render json: { data: {message: 'No search matches, please try again'} }, status: 400
     elsif params[:name] == nil
       render json: { data: {message: 'No search matches, please try again'} }, status: 400
     else
