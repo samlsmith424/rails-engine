ec6228f7e5e667542426c7050af8c0debb07967f
Sam Smith <samlsmith424@gmail.com>
2022-05-20 13:16:01 -0700

    Feat: delete commented out code

--- app/controllers/api/v1/merchants/most_items_controller.rb
+++ app/controllers/api/v1/merchants/most_items_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Merchants::MostItemsController < ApplicationController
   def index
     if params[:quantity] == ""
-      render json: {}, status: :bad_request
+      render json: { error: "bad request" }, status: :bad_request
     elsif !params.keys.include?("quantity")
-      render json: { error: "bad request"}, status: :bad_request
+      render json: { error: "bad request" }, status: :bad_request
     else
       number = params[:quantity]
       merchants = Merchant.merchants_by_item_quantity(number)
